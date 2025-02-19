aa4f1d8d7355c625c4861c646538e7c67ee00554
Albert Foster <kanon.gr2006@gmail.com>
2025-02-19 14:26:20 -0300

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,116 +1,85 @@
-import React, { useRef, useState } from 'react';
-// Import Swiper React components
-import { Swiper, SwiperSlide } from 'swiper/react';
-import Card from "./components/Card";
+import React, { useRef, useState } from "react";
+import 'keen-slider/keen-slider.min.css'
+import KeenSlider from 'keen-slider'
+import { useKeenSlider } from "keen-slider/react"
+import "keen-slider/keen-slider.min.css"
+import "./styles.css"
 
-// Import Swiper styles
-import 'swiper/css';
-import 'swiper/css/effect-coverflow';
-import 'swiper/css/pagination';
-
-import './App.css';
-
-// import required modules
-import { EffectCoverflow, Pagination } from 'swiper/modules';
 
 import bio1A from "./assets/images/cards/Bio1A.jpg";
-import histograma1A from "./assets/images/cards/histograma1A.webp";
-import clip2 from "./assets/images/cards/Clip2.webp";
+import Card from "./components/Card";
 
-
+const carousel = (slider) => {
+  const z = 300;
+  function rotate() {
+    const deg = 360 * slider.track.details.progress;
+    slider.container.style.transform = `translateZ(-${z}px) rotateY(${-deg}deg)`;
+  }
+  slider.on("created", () => {
+    const deg = 360 / slider.slides.length;
+    slider.slides.forEach((element, idx) => {
+      element.style.transform = `rotateY(${deg * idx}deg) translateZ(${z}px)`;
+    });
+    rotate();
+  });
+  slider.on("detailsChanged", rotate);
+};
 
 export default function App() {
-  return (
-    <>
-      <Swiper
-        effect={'coverflow'}
-        grabCursor={true}
-        centeredSlides={true}
-        slidesPerView={'auto'}
-        coverflowEffect={{
-          rotate: 50,
-          stretch: 0,
-          depth: 100,
-          modifier: 1,
-          slideShadows: true,
-        }}
-        pagination={true}
-        modules={[EffectCoverflow, Pagination]}
-        className="mySwiper"
-      >
-        <SwiperSlide>
-        <div className="embla__slide flex-shrink-0 min-w-[100px]">
-                <Card
-                  title="Resumen Biográfico"
-                  imgSrc={bio1A}
-                  h1="¡De generación en generación!"
-                  description="Armá la línea familiar, para conocer los protagonistas de una generación a otra y descubrir en la transmisión de esa herencia del pasado, tradiciones arraigadas que dan muchas veces sentido al hoy y al mañana."
-                />
-              </div>
-        </SwiperSlide>
-        <SwiperSlide>
-                        <div className="embla__slide flex-shrink-0 min-w-[100px]">
-                <Card
-                  title="Resumen Biográfico"
-                  imgSrc={bio1A}
-                  h1="¡De generación en generación!"
-                  description="Armá la línea familiar, para conocer los protagonistas de una generación a otra y descubrir en la transmisión de esa herencia del pasado, tradiciones arraigadas que dan muchas veces sentido al hoy y al mañana."
-                />
-              </div>
-        </SwiperSlide>
-        <SwiperSlide>
-                        <div className="embla__slide flex-shrink-0 min-w-[100px]">
-                <Card
-                  title="Resumen Biográfico"
-                  imgSrc={bio1A}
-                  h1="¡De generación en generación!"
-                  description="Armá la línea familiar, para conocer los protagonistas de una generación a otra y descubrir en la transmisión de esa herencia del pasado, tradiciones arraigadas que dan muchas veces sentido al hoy y al mañana."
-                />
-              </div>
-        </SwiperSlide>
-        <SwiperSlide>
-                        <div className="embla__slide flex-shrink-0 min-w-[100px]">
-                <Card
-                  title="Resumen Biográfico"
-                  imgSrc={bio1A}
-                  h1="¡De generación en generación!"
-                  description="Armá la línea familiar, para conocer los protagonistas de una generación a otra y descubrir en la transmisión de esa herencia del pasado, tradiciones arraigadas que dan muchas veces sentido al hoy y al mañana."
-                />
-              </div>
-        </SwiperSlide>
-        <SwiperSlide>
-                        <div className="embla__slide flex-shrink-0 min-w-[100px]">
-                <Card
-                  title="Resumen Biográfico"
-                  imgSrc={bio1A}
-                  h1="¡De generación en generación!"
-                  description="Armá la línea familiar, para conocer los protagonistas de una generación a otra y descubrir en la transmisión de esa herencia del pasado, tradiciones arraigadas que dan muchas veces sentido al hoy y al mañana."
-                />
-              </div>
-        </SwiperSlide>
-        <SwiperSlide>
-                        <div className="embla__slide flex-shrink-0 min-w-[100px]">
-                <Card
-                  title="Resumen Biográfico"
-                  imgSrc={bio1A}
-                  h1="¡De generación en generación!"
-                  description="Armá la línea familiar, para conocer los protagonistas de una generación a otra y descubrir en la transmisión de esa herencia del pasado, tradiciones arraigadas que dan muchas veces sentido al hoy y al mañana."
-                />
-              </div>
-        </SwiperSlide>
-        <SwiperSlide>
-                        <div className="embla__slide flex-shrink-0 min-w-[100px]">
-                <Card
-                  title="Resumen Biográfico"
-                  imgSrc={bio1A}
-                  h1="¡De generación en generación!"
-                  description="Armá la línea familiar, para conocer los protagonistas de una generación a otra y descubrir en la transmisión de esa herencia del pasado, tradiciones arraigadas que dan muchas veces sentido al hoy y al mañana."
-                />
-              </div>
-        </SwiperSlide>
-        
+  const [sliderRef] = useKeenSlider(
+    {
+      loop: true,
+      selector: ".carousel__cell",
+      renderMode: "custom",
+      mode: "free-snap",
+    },
+    [carousel]
+  );
 
-      </Swiper>
-    </>
+  return (
+    <div className="wrapper">
+      <div className="scene">
+        <div className="carousel keen-slider" ref={sliderRef}>
+          <div className="carousel__cell number-slide1 ">
+          <Card
+title="Resumen Biográfico"
+imgSrc={bio1A}
+h1="¡De generación en generación!"
+description="Armá la línea familiar, para conocer los protagonistas de una generación a otra y descubrir en la transmisión de esa herencia del pasado, tradiciones arraigadas que dan muchas veces sentido al hoy y al mañana."
+/>
+          </div>
+          <div className="carousel__cell number-slide2"><Card
+title="Resumen Biográfico"
+imgSrc={bio1A}
+h1="¡De generación en generación!"
+description="Armá la línea familiar, para conocer los protagonistas de una generación a otra y descubrir en la transmisión de esa herencia del pasado, tradiciones arraigadas que dan muchas veces sentido al hoy y al mañana."
+/></div>
+          <div className="carousel__cell number-slide3"><Card
+title="Resumen Biográfico"
+imgSrc={bio1A}
+h1="¡De generación en generación!"
+description="Armá la línea familiar, para conocer los protagonistas de una generación a otra y descubrir en la transmisión de esa herencia del pasado, tradiciones arraigadas que dan muchas veces sentido al hoy y al mañana."
+/></div>
+          <div className="carousel__cell number-slide4"><Card
+title="Resumen Biográfico"
+imgSrc={bio1A}
+h1="¡De generación en generación!"
+description="Armá la línea familiar, para conocer los protagonistas de una generación a otra y descubrir en la transmisión de esa herencia del pasado, tradiciones arraigadas que dan muchas veces sentido al hoy y al mañana."
+/></div>
+          <div className="carousel__cell number-slide5"><Card
+title="Resumen Biográfico"
+imgSrc={bio1A}
+h1="¡De generación en generación!"
+description="Armá la línea familiar, para conocer los protagonistas de una generación a otra y descubrir en la transmisión de esa herencia del pasado, tradiciones arraigadas que dan muchas veces sentido al hoy y al mañana."
+/></div>
+          <div className="carousel__cell number-slide6"><Card
+title="Resumen Biográfico"
+imgSrc={bio1A}
+h1="¡De generación en generación!"
+description="Armá la línea familiar, para conocer los protagonistas de una generación a otra y descubrir en la transmisión de esa herencia del pasado, tradiciones arraigadas que dan muchas veces sentido al hoy y al mañana."
+/></div>
+        </div>
+      </div>
+    </div>
   );
 }
